b42a7c857aa5fac89a25805e9e24b3be47e6096a
Jose Palma <jcpalma@jcpalma-mac.local>
2019-09-08 21:59:33 -0600

    Creacion de estructura y componentes basicos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { GraphComponent } from './pages/graph/graph.component';
+import { Page404Component } from './shared/page404/page404.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbComponent } from './shared/breadcrumb/breadcrumb.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    Page404Component,
+    DashboardComponent,
+    ProgressComponent,
+    GraphComponent,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbComponent
   ],
   imports: [
     BrowserModule,
